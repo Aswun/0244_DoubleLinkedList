5bfa5bdc2262ecd5f2f0e5f3fc2a5cd3f75a3490
Aswun <aswinlutfian32@gmail.com>
2024-05-02 15:55:27 +0700

    malanjutkan addNode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -19,4 +19,22 @@ void addNode()
     cin >> newNode -> noMhs;   // assign value to the data field of the new node
     cout << "/nEnter the name of the student : ";
     cin >> newNode -> name;   // assign value to the ddata field of the new node
+
+    // Insert the new node in the list
+    if (START != NULL || newNode -> noMhs <= START -> noMhs)
+    {
+        if (START != NULL && newNode -> noMhs == START -> noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed \033[0m" << endl;
+            return;
+        }
+        // If the list is empty, make the new node the START
+        newNode -> next = START;   // step 3 : make the first node point to the new node
+        if (START != NULL)
+        {
+            START -> prev = newNode;   // step 4 : make the first node point to the new node
+        } 
+        newNode -> prev = NULL;   // step 5 : make the new node point to NULL
+        START = newNode;   // step 6 : make the new node the first node
+    }
 }
